3d3d270d3397cf5638e85d7f865e39ef0574b6a8
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2024-04-03 16:35:24 +0900

    nodejs 크롤링 서버 세팅

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
