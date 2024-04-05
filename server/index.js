66dd8a3591f0f0eb9431f95b3aedf0d82dc2f6e7
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2024-04-05 17:27:35 +0900

    server setting 변경

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/**
 const express = require('express')
 const app = express()
 const port = 3000
@@ -37,3 +38,20 @@ getHtml()
     return data
   })
   .then((res) => log(res))
+*/
+
+const express = require('express')
+const path = require('path') // path 모듈 사용
+const app = express()
+
+app.listen(8080, () => {
+  console.log('server on')
+})
+
+app.use('/', express.static(path.join(__dirname, 'app/dist')))
+// 이 부분이 없으면 아래코드에서 index.html을 로드하지 못한다.
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app/dist/index.html'))
+})
+// 기본 경로 '/'을 통해 빌드된 dist/index.html 파일을 로드시킨다.
