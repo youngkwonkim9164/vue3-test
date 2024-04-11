d1dc6c476b2ef67bb6e349fdca7d2848c8f6a9f0
youngkwonkim9164 <youngkwonkim9164@gmail.com>
2024-04-11 10:51:27 +0900

    vue3 script 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,41 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+import { ref, onMounted } from 'vue'
+
+const count = ref(0)
+const str = ref('count 10 seconds! : ')
+
+const countdown = ref(10)
+
+let intervalId = null
+
+function startCountdown() {
+  countdown.value = 10
+  intervalId = setInterval(() => {
+    countdown.value--
+    if (countdown.value === 0) {
+      clearInterval(intervalId)
+    }
+  }, 1000)
+}
+
+function endCountdown() {
+  if (intervalId) {
+    clearInterval(intervalId)
+    intervalId = null
+  }
+  countdown.value = 0
+}
+
+onMounted(() => {})
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
+  <button @click="startCountdown">start</button> <br />
+  <button @click="endCountdown">end</button> <br />
+  <span>{{ str }} {{ countdown === 0 ? 'finish!' : countdown }} </span>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
